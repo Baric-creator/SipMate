b99047924b5335be23ba781d90fc20833024292c
Baric-creator <baric5555baric@gmail.com>
2026-09-09 23:25:32 +0200

    Hide support bubble in chat

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -62,7 +62,8 @@ export default function RootLayout() {
     currentRoute === 'register' ||
     currentRoute === 'onboarding' ||
     currentRoute === 'forgot-password' ||
-    currentRoute === 'reset-password';
+    currentRoute === 'reset-password' ||
+    currentRoute === 'chat';
 
   return (
     <>
